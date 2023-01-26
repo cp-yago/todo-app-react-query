4db079afe5efdb149db45b844c46d642c7464baa
cp-yago <yago.cunha123@gmail.com>
2023-01-25 23:26:35 -0300

    style: add styled components and configuring global theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './assets/styles/themes/default'
+import GlobalStyles from './assets/styles/global'
+
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ fontFamily: 'Inter' }}>Hello React!</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <div className="App">
+        <h1 style={{ fontFamily: 'Inter' }}>Hello React!</h1>
+      </div>
+    </ThemeProvider>
   )
 }
 
---
+++ src/assets/styles/themes/default.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+export const defaultTheme: DefaultTheme =  {
+  colors: {
+    background: '#1A1A1A',
+    primary: {
+      lighter: '#8284FA',
+      light: '#5E60CE',
+      main: '#4EA8DE',
+      dark: '#1E6F9F',
+    },
+    gray: {
+      900: '#222222',
+      400: '#333333',
+      300: '#808080'
+    },
+    black:{
+      900: '#0D0D0D'
+    },
+  },
+};
