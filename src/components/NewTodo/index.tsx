54dc2c147ee74efe743bcf1de2cfc8afe2d5a781
cp-yago <yago.cunha123@gmail.com>
2023-02-10 09:12:23 -0300

    refactor: optimistic update on add new task

--- src/components/NewTodo/index.tsx
+++ src/components/NewTodo/index.tsx
@@ -1,19 +1,31 @@
 import { Container, TextInput, Button } from './styles';
 import { AiOutlinePlusCircle } from 'react-icons/ai'
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postTask, Task } from '../../services/api';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 
 export const NewTodo = () => {
   const [todo, setTodo] = useState('')
-
+  const queryClient = useQueryClient()
   const clearTodoInput = () => setTodo('')
 
   const { mutate: createTask } = useMutation({
     mutationFn: postTask,
-    onSuccess: () => {
+    onMutate: async (newTask: Task) => {
       clearTodoInput();
+      await queryClient.cancelQueries({ queryKey: ['tasks'] })
+      const previousTasks = queryClient.getQueryData(['tasks']) || []
+      queryClient.setQueryData<Task[]>(['tasks'], (old: Task[] | undefined) => [...old as Task[], newTask])
+      return { previousTasks };
+    },
+    onError: (err, newTodo, context) => {
+      queryClient.setQueryData(['tasks'], context?.previousTasks)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] })
+    },
+    onSuccess: () => {
       toast.success('New task added!', {
         position: "bottom-center",
         autoClose: 2000,
@@ -25,7 +37,7 @@ export const NewTodo = () => {
         theme: "dark",
       });
     }
-  })
+  });
 
   const handleAddTask = () => {
     createTask({
